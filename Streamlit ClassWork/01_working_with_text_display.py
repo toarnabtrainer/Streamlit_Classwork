4998f6de2f5c35a1bcb89b4cf1050201ce19bd0f
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-09-07 21:31:07 +0530

    Update 01_working_with_text_display.py

--- Streamlit ClassWork/01_working_with_text_display.py
+++ Streamlit ClassWork/01_working_with_text_display.py
@@ -25,12 +25,18 @@ st.markdown("# This is a Markdown heading")
 st.markdown("[Markdown Cheat Sheet](https://www.markdownguide.org/cheat-sheet/)")
 st.markdown("This is a Markdown paragraph with **bold** and *italic* text")
 st.markdown("""
-        1. step 1
-        2. step 2
+        
+	1. Artificial Intelligence
+		1. Machine Learning
+		2. Deep Learning
+		3. Reinforcement Learning
+	2. Data Visualization
             
-        - unordered step 1
-        - step 2
-            - substep 2.1    
+	* Artificial Intelligence
+		- Machine Learning
+		- Deep Learning
+		- Reinforcement Learning
+	* Data Visualization
 """)
 
 # Emojis
@@ -40,12 +46,18 @@ st.markdown("Here are some emojis:")
 st.markdown(":thumbsup: :heart: :rocket: :smile:")
 
 # HTML
-st.markdown("### HTML")
+st.markdown("# HTML - Heading 1")
+st.markdown("## HTML - Heading 2")
+st.markdown("### HTML - Heading 3")
+st.markdown("#### HTML - Heading 4")
+st.markdown("##### HTML - Heading 5")
+st.markdown("###### HTML - Heading 6")
+
 html_code = """
         <h1 style='color: blue;'>This is a blue heading</h1>
         <p style='color: green;'>This is a green paragraph</p>
 """
-st.markdown(html_code, unsafe_allow_html=True)
+st.markdown(html_code, unsafe_allow_html = True)
 
 # Divider
 st.markdown("""---""")
